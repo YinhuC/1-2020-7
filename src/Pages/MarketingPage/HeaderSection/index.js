3aba95e98367bdbbc4ecfddfa9b4074505672fba
YinhuC <yinhucui@hotmail.com>
2020-11-05 05:31:22 +1300

    Add custom cursor

--- src/Pages/MarketingPage/HeaderSection/index.js
+++ src/Pages/MarketingPage/HeaderSection/index.js
@@ -3,12 +3,13 @@ import React from "react";
 
 /* Components */
 import { BackgroundImage } from "./style";
-
+import { Cursor } from "../../../Components/Cursor/index";
 /* Functions */
 
 function HeaderSection() {
   return (
     <>
+      <Cursor />
       <BackgroundImage />
     </>
   );
