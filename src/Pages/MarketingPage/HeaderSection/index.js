ed767e64eabe593d3d8a3dad7fb202016a9a7637
YinhuC <yinhucui@hotmail.com>
2020-11-04 22:58:24 +1300

    Setup marketing page

---
+++ src/Pages/MarketingPage/HeaderSection/index.js
@@ -0,0 +1,18 @@
+/* Third Party */
+import React from "react";
+
+/* Components */
+
+/* Functions */
+
+function HeaderSection() {
+  return (
+    <>
+      <div>
+        <p>hello</p>
+      </div>
+    </>
+  );
+}
+
+export default HeaderSection;
---
+++ src/Pages/MarketingPage/index.js
@@ -0,0 +1,17 @@
+/* Third Party */
+import React from "react";
+
+/* Components */
+import HeaderSection from "./HeaderSection";
+
+/* Functions */
+
+function MarketingPage() {
+  return (
+    <>
+      <HeaderSection />
+    </>
+  );
+}
+
+export default MarketingPage;
