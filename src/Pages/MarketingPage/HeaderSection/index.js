4ab4a80c26b9b160d0ac6f9f737efd52937a8903
YinhuC <yinhucui@hotmail.com>
2020-11-30 22:29:22 +1300

    Add video to background

--- src/Pages/MarketingPage/HeaderSection/index.js
+++ src/Pages/MarketingPage/HeaderSection/index.js
@@ -2,27 +2,39 @@
 import React from "react";
 
 /* Components */
-import { BackgroundImage, HeaderContainer } from "./style";
+import {
+  BackgroundContainer,
+  HeaderContainer,
+  BackgroundVideo,
+  VideoContainer,
+} from "./style";
 import { Cursor } from "../../../Components/Cursor/index";
 import CursorManager from "../../../Components/Cursor/Context/manager";
 import { MenuIcon } from "../../../Components/Icons/MenuIcon";
+import video from "../../../Videos/dark.mp4";
 
 /* Functions */
 
 function HeaderSection() {
   return (
-    <>
-      <CursorManager>
-        {/* need to actually wrap the icon and cursor for it to change */}
-        <Cursor />
-        <BackgroundImage>
-          <HeaderContainer>
-            Explore New Zealand
-            <MenuIcon />
-          </HeaderContainer>
-        </BackgroundImage>
-      </CursorManager>
-    </>
+    <div>
+      <VideoContainer>
+        <BackgroundVideo autoPlay muted loop id="myVideo">
+          <source src={video} type="video/mp4" />
+        </BackgroundVideo>
+        <CursorManager>
+          {/* need to actually wrap the icon and cursor for it to change */}
+          <Cursor />
+          <BackgroundContainer>
+            <HeaderContainer>
+              Explore New Zealand
+              <MenuIcon />
+            </HeaderContainer>
+          </BackgroundContainer>
+        </CursorManager>
+      </VideoContainer>
+      <BackgroundContainer />
+    </div>
   );
 }
 
--- src/Pages/MarketingPage/HeaderSection/style.js
+++ src/Pages/MarketingPage/HeaderSection/style.js
@@ -6,6 +6,57 @@ import image3 from "../../../Images/0013-Springfield-Canterbury-Matt-Crawford-mi
 import image4 from "../../../Images/8399-Wanaka-Miles-Holden-min.jpg";
 import image5 from "../../../Images/45-Aoraki-Lake-Pukaki-Will-Patino-min.jpg";
 
+export const BackgroundVideo = styled.video`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  object-fit: cover;
+  z-index: -1;
+`;
+
+export const VideoContainer = styled.div`
+  /* Make video to at least 100% wide and tall */
+  min-width: 100%;
+  min-height: 100%;
+
+  /* Setting width & height to auto prevents the browser from stretching or squishing the video */
+  width: auto;
+  height: auto;
+
+  /* Center the video */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const BackgroundContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  /* Full height */
+  height: 100vh;
+  width: 100%;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  user-select: none;
+
+  width: 100%;
+
+  color: #fff;
+  font-weight: bolder;
+  font-size: 30px;
+  margin: 6% 10% 6% 10%;
+`;
+
 const animate = keyframes`
   /* Background Images, preloading to avoid flicker*/
   0% {
@@ -23,24 +74,12 @@ const animate = keyframes`
   }
 `;
 
-export const BackgroundImage = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-
-  /* The image used */
-  background-image: url(${image});
-
-  /* Full height */
-  height: 100vh;
-  width: 100%;
-
-  /* Center and scale the image nicely */
-  background-position: center;
+/* Center and scale the image nicely */
+/* background-position: center;
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: cover; */
 
-  /* Change background Image
+/* Change background Image
   transition: 5s;
 
   animation-name: ${animate};
@@ -50,18 +89,3 @@ export const BackgroundImage = styled.div`
   animation-iteration-count: infinite;
   animation-play-state: running;
   animation-timing-function: ease-in-out; */
-`;
-
-export const HeaderContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  user-select: none;
-
-  width: 100%;
-
-  color: #fff;
-  font-weight: bolder;
-  font-size: 30px;
-  margin: 6% 10% 6% 10%;
-`;
