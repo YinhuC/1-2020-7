9535874c307d71b7955e4545c73759738fdd5bc5
Yinhu Cui <yinhuc@orionhealth.com>
2021-01-12 16:26:14 +1300

    Re-structure router and location pages

--- src/App.js
+++ src/App.js
@@ -9,20 +9,12 @@ import {
 
 /* Components */
 import MarketingPage from './Pages/MarketingPage';
+import LocationPages from './Pages/LocationPages';
 import CursorManager from './Components/Cursor/Context/manager.tsx';
 import { Cursor } from './Components/Cursor';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Error from './Pages/ErrorPage';
-import Taupo from './Pages/LocationPages/Taupo';
-import Auckland from './Pages/LocationPages/Auckland';
-import Coromandel from './Pages/LocationPages/Coromandel';
-import Rotorua from './Pages/LocationPages/Rotorua';
-import Ruapehu from './Pages/LocationPages/Ruapehu';
-import Christchurch from './Pages/LocationPages/Christchurch';
-import Marlborough from './Pages/LocationPages/Marlborough';
-import Queenstown from './Pages/LocationPages/Queenstown';
-import Wanaka from './Pages/LocationPages/Wanaka';
 
 /* Functions */
 
@@ -39,33 +31,7 @@ function App() {
             <Header />
             <MarketingPage />
           </Route>
-          <Route path='/location/rotorua'>
-            <Rotorua />
-          </Route>
-          <Route path='/location/taupo'>
-            <Taupo />
-          </Route>
-          <Route path='/location/auckland'>
-            <Auckland />
-          </Route>
-          <Route path='/location/coromandel'>
-            <Coromandel />
-          </Route>
-          <Route path='/location/ruapehu'>
-            <Ruapehu />
-          </Route>
-          <Route path='/location/christchurch'>
-            <Christchurch />
-          </Route>
-          <Route path='/location/marlborough'>
-            <Marlborough />
-          </Route>
-          <Route path='/location/queenstown'>
-            <Queenstown />
-          </Route>
-          <Route path='/location/wanaka'>
-            <Wanaka />
-          </Route>
+          <LocationPages />
           <Route path='*'>
             <Error />
           </Route>
