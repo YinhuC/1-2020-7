3cd0f67dc33df62e8a351cdf20875f6c34040bf3
Yinhu <41180818+YinhuC@users.noreply.github.com>
2021-11-20 23:18:55 +1300

    Install gsap

--- src/Pages/MarketingPage/HeaderSection/index.js
+++ src/Pages/MarketingPage/HeaderSection/index.js
@@ -1,6 +1,6 @@
 /* Third Party */
-import React from "react";
-import { Row, Col } from "reactstrap";
+import React from 'react';
+import { Row, Col } from 'reactstrap';
 
 /* Components */
 import {
@@ -9,9 +9,9 @@ import {
   BackgroundVideo,
   VideoContainer,
   MainContainer,
-} from "./style";
-import video from "../../../Videos/dark.mp4";
-import { DownArrow } from "../../../Components/Icons/DownArrow";
+} from './style';
+import video from '../../../Videos/dark.mp4';
+import { DownArrow } from '../../../Components/Icons/DownArrow';
 
 /* Functions */
 /* need to actually wrap the icon AND cursor for it to change, also need 
@@ -21,25 +21,25 @@ function HeaderSection() {
   return (
     <>
       <VideoContainer>
-        <BackgroundVideo autoPlay muted loop id="myVideo">
-          <source src={video} type="video/mp4" />
+        <BackgroundVideo autoPlay muted loop id='myVideo'>
+          <source src={video} type='video/mp4' />
         </BackgroundVideo>
         <BackgroundContainer>
           <MainContainer>
             <Row></Row>
             <Row>
-              <Col className="col-lg-7">
-                <HeaderContainer className="d-flex d-lg-none justify-content-center flex-column text-center">
+              <Col className='col-lg-7'>
+                <HeaderContainer className='d-flex d-lg-none justify-content-center flex-column text-center'>
                   Discover <br />
                   New Zealand
-                  <div style={{ fontSize: "30px" }}>Begin Your Journey</div>
+                  <div style={{ fontSize: '30px' }}>Begin Your Journey</div>
                 </HeaderContainer>
-                <HeaderContainer className="d-none d-lg-flex flex-column">
+                <HeaderContainer className='d-none d-lg-flex flex-column'>
                   Discover New Zealand
-                  <div style={{ fontSize: "30px" }}>Begin Your Journey</div>
+                  <div style={{ fontSize: '30px' }}>Begin Your Journey</div>
                 </HeaderContainer>
               </Col>
-              <Col className="col-lg-5 d-none d-lg-flex justify-content-end mt-2 ">
+              <Col className='col-lg-5 d-none d-lg-flex justify-content-end mt-2 '>
                 <DownArrow />
               </Col>
             </Row>
