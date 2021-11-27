0665e073b02ca65c518404db86265fdf4cb0ae3d
Yinhu <41180818+YinhuC@users.noreply.github.com>
2021-11-27 23:01:36 +1300

    Update marketing page fonts

--- src/Pages/MarketingPage/SlideSection/style.js
+++ src/Pages/MarketingPage/SlideSection/style.js
@@ -49,3 +49,10 @@ export const VerticalContainer = styled.div`
     padding: 0 5% 0 5%;
   }
 `;
+
+export const BumperText = styled.div`
+  font-family: 'Playfair Display', serif;
+  font-size: 50px;
+  font-weight: 400;
+  color: black;
+`;
